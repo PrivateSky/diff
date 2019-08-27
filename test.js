bf6be410d2d82d33cebbcc0b29a9be8fcfd92739
BaditaGeorge <baditageorge@yahoo.com>
2019-08-27 11:17:30 +0300

    fixed some problems in diff representation

--- test.js
+++ test.js
@@ -1,23 +1,28 @@
 const fs = require("fs");
 
-let obj2 = {
+let obj2 = { //acolada 1
     propNum: 100,
     propStr:"string 100",
-    propArr:[1,"100"],
-    propObj:{
+    propArr:[//acolada2
+        1,"100"],
+    propObj:{ //acolada3
         propNum:101,
+        //lipseste complet obiectul propFs din cadrul parantezei cu numarul 3
         propStr:"string 101",
         redundantPropStr:"string 101",
-        propArr:[1,"100",{
+        propArr:[ //acolada4
+            1,"100",{ //acolada5
             propNum:102,
-            propStr:"string 103"
+            propStr:"string 103" //obiectul propStr din cadrul parantezei numarul 5 are valoarea modificata
+            //lipseste complet obiectul propstar din cadrul parantezei numarul 5
         }]
     },
-    minobj:{
+    minobj:{ //acolada6
         duo:3,
         uno:2
     },
-    arr:[1,2,3,4,5,6,7,'a']
+    arr:[ //acolada7
+        1,2,3,4,5,6,7,'a'] //aici, in cadrul parantezei numarul 7, a-ul s-a transformat in b, si lipseste c-ul
 };
 let obj3 = {
     propNum: 100,
@@ -40,8 +45,7 @@ let obj3 = {
     },
     arr:[1,2,3,4,5,6,7,'b','c']
 };
-let diff = [3,['d',{propFs:"LLT"}],5,['d',{propstar:"HM"}],7,[['m','a','b'],['d','c']]];
-let diff2 = '[3,["d",{"propFs":"LLT"}],5,["d",{"propstar":"HM"}],7,[["m","a","b"],["d","c"]]]';
+let diff2 = '[3,["d",{"propFs":"LLT"}],5,[["d",{"propstar":"HM"}],["m",2,"string 103","string 101"]],7,[["m",8,"a","b"],["d","c"]]]';
 // console.log(obj3);
 // console.log(JSON.stringify(diff));
 console.log(JSON.parse(diff2)[1][1]);
