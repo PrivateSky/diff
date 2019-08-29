53fcb1b4f55077a14230512e70692e23dab43d61
BaditaGeorge <baditageorge@yahoo.com>
2019-08-29 14:42:09 +0300

    added tests, and refactored code for Patcher

--- tests/testPatch.js
+++ tests/testPatch.js
@@ -1,24 +1,49 @@
 process.env.NO_LOGS = "true";
 require("../bundles/pskruntime");
-const Patch = require("../lib/Patch");
+const Patcher = require("../lib/Patcher");
 
 const fs = require('fs');
 const assert = require('double-check').assert;
 
 let textEx = ['ana are mere','ana are mere, pere','ana are mere si pere'];
-let diffs = ['["+",8,"pere verzi si"]','[8,1,"p",14,1,"m"]','["-",8,8]'];
+let diffs = ['["+",8,"pere verzi si "]','[8,1,"p",14,1,"m"]','["-",8,8]'];
 let patched = ['ana are pere verzi si mere','ana are pere, mere','ana are pere'];
 assert.begin("testPath", () => {
     console.log("Cleabnup")
 }, 3000);
 
 assert.callback("testPath",(callback)=>{
-    for(let index = 0; index< 1; index++){
-        fs.writeFileSync('../tests/a.txt',textEx[index]);
-        fs.writeFileSync('../tests/b.txt',diffs[index]);
-        Patch('b.txt','a.txt');
-        console.log(fs.readFileSync('a.txt').toString());
-        assert.true(1 === 1);
+    function smallRecursion(index,length){
+        if(index>=length){
+            return callback(undefined);
+        }
+        fs.writeFile('a.txt',textEx[index],(err)=>{
+            if(err){
+                return callback(err);
+            }
+            fs.writeFile('b.txt',diffs[index],(err)=>{
+                if(err){
+                    return callback(err);
+                }
+                new Patcher().callPatcher('b.txt','a.txt',(err,data)=>{
+                    if(err){
+                        return callback(err);
+                    }
+                    assert.true(data === patched[index]);
+                    fs.unlink('a.txt',(err)=>{
+                        if(err){
+                            return callback(err);
+                        }
+                        fs.unlink('b.txt',(err)=>{
+                            if(err){
+                                return callback(err);
+                            }
+                            smallRecursion(index+1,length);
+                        });
+                    });
+                });
+            });
+        });
     }
-    callback();
+    smallRecursion(0,3);
 },3000);
