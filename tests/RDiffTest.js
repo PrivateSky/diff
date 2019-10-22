76f99fb0856462f8fd831dc1eac728a5cddec4fe
BaditaGeorge <baditageorge@yahoo.com>
2019-10-22 14:40:00 +0300

    rsync algorithm for diffs

---
+++ tests/RDiffTest.js
@@ -0,0 +1,43 @@
+process.env.NO_LOGS = "true";
+require("../bundles/pskruntime");
+const Diff = require("../lib/Diff.js");
+const Patcher = require("../lib/Patcher.js");
+const RDiff = require("../lib/RsyncDiff.js");
+
+const fs = require('fs');
+const assert = require('double-check').assert;
+
+let jsonBigEx = '{"propNum":100,"propStr":"string 100","propArr":[1,"100"],"propObj":{"propNum":101,"propStr":"string 101","redundantPropStr":"string 101","propArr":[1,"100",{"propNum":102,"propStr":"string 103"}]}}';
+let jsonBigExM = '{"propNum":100,"propStr":"string 100","propArr":[1,"100"],"propObj":{"propNum":101,"propStr":"string 101","propArr":[1,"100",{"propNum":102,"propStr":"24"},70],"newProp":1007},"newNum":1001,"newProp":1002}';
+let textEx = ['ana are mere','ana are mere, pere','ana are mere si pere','{pop:3}','Ana are mere',jsonBigEx,'ana are mere'];
+let patched = ['ana are pere verzi si mere','ana are pere, mere','ana are pere','{pop:4,ob:"salut",valsViniez:{arr:[1,2,3]}}','Ana are pere verzi si mere',jsonBigExM,'ana are pere verzi'];
+
+assert.begin("testDiff",()=>{
+    console.log("Cleanup");
+},3000);
+
+assert.callback("testDiff",(callback)=>{
+    function testAllDiffs(){
+        for(let index = 0; index < textEx.length; index++){
+            //let diff = Diff().createDiff(textEx[index],patched[index]);
+            //let patchedData = Patcher().applyPatch(diff,textEx[index]);
+            let diff = RDiff().runRsync(textEx[index],5,patched[index]);
+            let patchedData = Patcher().applyPatch(diff,textEx[index]);
+            //console.log(patchedData);
+            assert.true(patchedData === patched[index]);
+        }
+        return callback(undefined);
+    }
+    testAllDiffs();
+},3000);
+
+// let ja = "Don't cry over, smile";
+// let jb = "Don't cry because it's over, smile because it happened";
+// let rsync = new RsyncDiff();
+//
+// //let diff = rsync.runRsync('ana are mere',8,'ana are pere verzi si mere');
+// let diff = rsync.runRsync(textEx[6],5,patched[6]);
+// console.log(diff);
+// let out = Patcher().applyPatch(diff,textEx[6]);
+// console.log(out);
+// console.log(out === patched[6]);
