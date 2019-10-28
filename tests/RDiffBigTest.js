360515e81c28c21e748c39fa76ff0f49b598eb15
BaditaGeorge <baditageorge@yahoo.com>
2019-10-28 14:47:23 +0200

    optimyzed algorithm, now i keep checksums in an object as keys, and i can find in logarithmic time matches

--- tests/RDiffBigTest.js
+++ tests/RDiffBigTest.js
@@ -31,14 +31,14 @@ assert.callback("testDiff",(callback)=>{
         //     position = Math.floor(Math.random()*picks.length);
         //     secondString += picks[position];
         // }
-        firstString = fs.readFileSync('cpy.js');
-        secondString = fs.readFileSync('og.js');
+        firstString = fs.readFileSync('nm2.txt');
+        secondString = fs.readFileSync('nm4.txt');
         firstString = firstString.toString();
         secondString = secondString.toString();
         let filePath = 'nm4.txt';
         // if (!fs.existsSync(filePath)) {
         //     const file = fs.createWriteStream(filePath);
-        //     for (let i = 0; i <= 3e3; i++) {
+        //     for (let i = 0; i <= 1e4; i++) {
         //         file.write('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n');
         //     }
         //
@@ -58,8 +58,10 @@ assert.callback("testDiff",(callback)=>{
         console.log(firstString.length);
         console.log(secondString.length);
         console.time('diff');
-        let diff = RDiff().runRsync(firstString,12288,secondString);
+        let diff = RDiff().runRsync(firstString,256,secondString);
+        console.log(diff.length);
         console.timeEnd('diff');
+        console.log(diff[0],diff[3]);
         let patchedData = Patcher().applyPatch(diff,firstString);
         //console.log(diff);
         console.log(patchedData === secondString);
